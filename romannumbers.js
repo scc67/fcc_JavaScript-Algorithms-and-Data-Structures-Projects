c897f5da4878f7f5afd9bf9b7d040396162d7a57
Steve <anonymouse@bestaatniet.com>
2020-07-24 00:01:12 +0200

    assignment is af

--- romannumbers.js
+++ romannumbers.js
@@ -1,35 +1,57 @@
-function convertToRoman(num) {
-    const one = 'X'
-    const five = 'L'
-    const ten = 'C'
-
-    function createRomanPart(oneRoman, fiveRoman, tenRoman) {
-        if (num === 0) {
-            return ''
-        } else if (num <= 3) {
-            return oneRoman.repeat(num);
-        } else if (num === 4) {
-            return oneRoman + fiveRoman
-        } else if (num === 5) {
-            return fiveRoman;
-        } else if (num <= 8) {
-            return fiveRoman + oneRoman.repeat(num - 5);
-        } else if (num === 9) {
-            return oneRoman + tenRoman;
+function convertToRoman(number) {
+    let romanNumber = '';
+    let one = '';
+    let five = '';
+    let ten = '';
+    let num = 0;
+    for (let i = 0; i < number.toString().length; i++) {
+        if (i === number.toString().length - 1) {
+            one = 'I'
+            five = 'V'
+            ten = 'X'
+        } else if (i === number.toString().length - 2) {
+            one = 'X'
+            five = 'L'
+            ten = 'C'
+        } else if (i === number.toString().length - 3) {
+            one = 'C'
+            five = 'D'
+            ten = 'M'
+        } else if (i === number.toString().length - 4) {
+            one = 'M'
+            five = '-'
+            ten = '_'
         }
-    }
-    let roman = createRomanPart(one, five, ten)
+        num = Number(String(number).charAt(i));
 
-    return roman;
+        function createRomanPart(oneRoman, fiveRoman, tenRoman, numRoman) {
+            if (numRoman === 0) {
+                return ''
+            } else if (numRoman <= 3) {
+                return oneRoman.repeat(numRoman);
+            } else if (numRoman === 4) {
+                return oneRoman + fiveRoman
+            } else if (numRoman === 5) {
+                return fiveRoman;
+            } else if (numRoman <= 8) {
+                return fiveRoman + oneRoman.repeat(numRoman - 5);
+            } else if (numRoman === 9) {
+                return oneRoman + tenRoman;
+            }
+        }
+        romanNumber = romanNumber + createRomanPart(one, five, ten, num)
+    }
+    return romanNumber;
+
 }
 
-console.log(convertToRoman(1));
-console.log(convertToRoman(2));
-console.log(convertToRoman(3));
-console.log(convertToRoman(4));
-console.log(convertToRoman(5));
-console.log(convertToRoman(6));
-console.log(convertToRoman(7));
-console.log(convertToRoman(8));
-console.log(convertToRoman(9));
-console.log(convertToRoman(0));
+// console.log(convertToRoman(111));
+// console.log(convertToRoman(222));
+// console.log(convertToRoman(333));
+// console.log(convertToRoman(444));
+// console.log(convertToRoman(555));
+// console.log(convertToRoman(666));
+// console.log(convertToRoman(777));
+// console.log(convertToRoman(888));
+console.log(convertToRoman(123));
+console.log(convertToRoman(3999));
